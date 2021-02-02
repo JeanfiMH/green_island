9ce5c85fc499a08db195725f41720cd9f706f29c
jeanfi mollheger <mollhegerjp@hotmail.com>
2021-02-02 20:42:32 +0100

    model villager validation and association

--- app/models/villager.rb
+++ app/models/villager.rb
@@ -1,3 +1,9 @@
 class Villager < ApplicationRecord
   belongs_to :user
+  has_many :reviews dependent: :destroy
+  has_many :bookings dependent: :destroy
+  validates :name, uniqueness: true, presence: true
+  validates :image_villager, uniqueness: true, presence: true
+  validates :description_villager, :image_house, :description_house, :specie, :gender, :personality, presence: true
+  
 end
