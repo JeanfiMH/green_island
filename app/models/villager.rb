01e3c8a46d9b8555c0cbe2026c9d803c50f3d1ec
jeanfi mollheger <mollhegerjp@hotmail.com>
2021-02-04 19:52:55 +0100

    end validation by inclusion

--- app/models/villager.rb
+++ app/models/villager.rb
@@ -1,9 +1,19 @@
 class Villager < ApplicationRecord
+  GENDERS = ["Male", "Female"]
+  validates :gender, inclusion: { in: GENDERS}
+
+  SPECIES = ["Cat", "Cub","Duck", "Deer", "Penguin", "Rabbit", "Hippo", "Eagle", "Elephant", "Dog"]
+  validates :specie, inclusion: { in: SPECIES}
+
+  PERSONALITIES = ["Lazy", "Jock","Snooty", "Peppy", "Cranky"]
+  validates :personality, inclusion: { in: PERSONALITIES}
+
   belongs_to :user
-  has_many :reviews dependent: :destroy
-  has_many :bookings dependent: :destroy
+  has_many :reviews, dependent: :destroy
+  has_many :bookings, dependent: :destroy
   validates :name, uniqueness: true, presence: true
   #validates :image_villager, uniqueness: true, presence: true
   validates :specie, :gender, :personality, presence: true
   #validates :description_villager, :image_house, :description_house, presence: true
+
 end
---
+++ db/migrate/20210204182254_add_validation_categorie_species_by_inclusion.rb
@@ -0,0 +1,5 @@
+class AddValidationCategorieSpeciesByInclusion < ActiveRecord::Migration[6.0]
+  def change
+    
+  end
+end
