9b876729099055228d671d26cf243f5abc6d10a6
Lea Destaillac <lea.destaillac@gmail.com>
2021-02-06 14:00:59 +0100

    seed avec nookipedia Api modif model villager

--- app/models/villager.rb
+++ app/models/villager.rb
@@ -2,18 +2,23 @@ class Villager < ApplicationRecord
   GENDERS = ["Male", "Female"]
   validates :gender, inclusion: { in: GENDERS}
 
-  SPECIES = ["Cat", "Cub","Duck", "Deer", "Penguin", "Rabbit", "Hippo", "Eagle", "Elephant", "Dog"]
+  SPECIES = ["Alligator", "Anteater", "Bear", "Bird", "Bull", "Cat", "Cub", "Chicken", "Cow", "Deer", "Dog", "Duck", "Eagle", "Elephant", "Frog", "Goat", "Gorilla", "Hamster", "Hippo", "Horse", "Koala", "Kangaroo", "Lion", "Monkey", "Mouse", "Octopus", "Ostrich", "Penguin", "Pig", "Rabbit", "Rhino", "Sheep", "Squirrel", "Tiger", "Wolf"]
   validates :specie, inclusion: { in: SPECIES}
 
-  PERSONALITIES = ["Lazy", "Jock","Snooty", "Peppy", "Cranky"]
+  PERSONALITIES = ["Lazy", "Jock", "Cranky", "Smug", "Normal", "Peppy", "Snooty", "Sisterly"]
   validates :personality, inclusion: { in: PERSONALITIES}
 
   belongs_to :user
   has_many :reviews, dependent: :destroy
   has_many :bookings, dependent: :destroy
+
   validates :name, uniqueness: true, presence: true
-  #validates :image_villager, uniqueness: true, presence: true
-  validates :specie, :gender, :personality, presence: true
-  #validates :description_villager, :image_house, :description_house, presence: true
+  validates :image_villager, presence: true
+  validates :specie, presence: true
+  validates :gender, presence: true
+  validates :personality, presence: true
+  validates :image_house, presence: true
+  # validates :description_villager, presence: true
+  # validates :description_house, presence: true
 
 end
