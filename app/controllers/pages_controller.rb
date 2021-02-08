a15c2775dcd4a1b1b0f35a89db3af9e16e4fca1d
Lea Destaillac <lea.destaillac@gmail.com>
2021-02-08 17:24:08 +0100

    navbar + modif routes & villagers controller + views new, edit + config css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
   def home
     @villagers = Villager.last(6)
     @specie_villagers = Villager.where(specie: "Cat")
--- app/controllers/villagers_controller.rb
+++ app/controllers/villagers_controller.rb
@@ -1,13 +1,58 @@
 class VillagersController < ApplicationController
+  before_action :set_villager, only: [:show, :edit, :update]
+  skip_before_action :authenticate_user!, only: :index
+
+  # GET /villagers
   def index
     @villagers = Villager.all
   end
-  
+
+  # GET /villagers/1
   def show
+
+  end
+
+  # GET /villagers/new
+  def new
+    @villager = Villager.new
+  end
+
+  # POST /villagers
+  def create
+    @villager = Villager.new(villager_params)
+    @villager.user = current.user
+
+    if @villager.save
+      redirect_to villager_path(@villager)
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+    @villager.update(villager_params)
+    redirect_to villager_path(@villager)
+  end
+
+  # DELETE /villagers/1
+  def destroy
+    @villager.destroy
+    redirect_to villagers_path, notice: 'Villager was successfully destroyed'
+  end
+
+private
+
+  def set_villager
     @villager = Villager.find(params[:id])
   end
 
-
-
+  def villager_params
+    params.require(:villager).permit(:name, :image_villager, :specie, :gender, :personality, :image_house, :user)
+  end
 
 end
