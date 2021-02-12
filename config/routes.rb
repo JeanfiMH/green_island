414856ac37a20f38221909099d31947f0db603cb
leellou <lea.destaillac@gmail.com>
2021-02-12 14:26:34 +0100

    routes views booking (edit/destroy/index)

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
     resources :villagers do
       resources :bookings, only: [ :new, :create ]
     end
-    
+
     resources :users do
-      resources :bookings, only: [ :show, :update ]
+      resources :bookings, only: [ :index, :update, :edit, :destroy ]
     end
-  
-end  
+
+end
 
